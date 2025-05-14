c57717b2c2e2299006d2a1d99bb64c1525b085bc
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-05-14 14:57:15 +0530

    Initialize Spring Boot project with Maven Wrapper and basic application structure

---
+++ src/main/java/dev/arghya/quotiva/QuotivaApplication.java
@@ -0,0 +1,13 @@
+package dev.arghya.quotiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arghya/quotiva/QuotivaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arghya.quotiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
