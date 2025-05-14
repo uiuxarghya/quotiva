be33273ea54bc3009a7d839741d3bda876834720
Arghya Ghosh <71373838+uiuxarghya@users.noreply.github.com>
2025-05-14 19:00:10 +0530

    feat(model): Add Quote model class with text and author fields

---
+++ src/main/java/dev/arghya/quotiva/model/Quote.java
@@ -0,0 +1,30 @@
+package dev.arghya.quotiva.model;
+
+public class Quote {
+    private String text;
+    private String author;
+
+    public Quote() {
+    }
+
+    public Quote(String text, String author) {
+        this.text = text;
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
